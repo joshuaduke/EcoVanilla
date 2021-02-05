a9b54a30730cebf1a248c366d8be9014b51d71a3
Joshua Duke <dukejoshuae@gmail.com>
2021-02-04 22:57:53 -0500

    Created simple node app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res)=>{
+    res.render('index'); //error
+} )
+
+app.listen(port, ()=>{
+    console.log(`This server is listening on Port: ${port}`);
+})
